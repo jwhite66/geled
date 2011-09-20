22061d256ac167bd54477a3908a641c0b40093d7
Jeremy White <jwhite@codeweavers.com>
2011-09-19 23:11:55 -0500

    Shift to the canonical timings.

--- led.h
+++ led.h
@@ -1,6 +1,4 @@
 // TODO - we're getting the occasional odd color bulb - why?
-// TODO - is the padding logic really needed?  Maybe not
-// TODO - the safe slices stuff is a bit on the guesstimate side
 
 #define MAX_BRIGHT          0xCC
 #define ADDR_COUNT          50
@@ -8,7 +6,7 @@
 #define ALL_STRINGS_MASK    0x3F
 
 
-#define SLICE_LEN_IN_MICROS 5
+#define SLICE_LEN_IN_MICROS 10
 #define CYCLES_PER_SLICE    ((F_CPU / 1000000) * SLICE_LEN_IN_MICROS)
     /* 16,000,000 cycles / sec == 16 cyles / us */
     /*  For one interrupt every 10 us, we'd get 160 */
@@ -16,11 +14,10 @@
 #define MICROS_PER_SLICE    ((CYCLES_PER_SLICE * 1000000) / F_CPU)
 
 
-#define START_SLICES        3
+#define START_SLICES        1
+#define STOP_SLICES         3
 #define SLICES_PER_BIT      3
 #define BITS_PER_BULB       26
-
-#define STOP_SLICES       3
 #define SLICES_TO_SHOW_BULB (START_SLICES + (SLICES_PER_BIT * BITS_PER_BULB) + STOP_SLICES)
 
 
