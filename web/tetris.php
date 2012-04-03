ad3e97c2097ea40ded24c7211c872c174092289c
Jeremy White <jwhite@codeweavers.com>
2012-04-03 17:00:41 -0500

    Provide a way to start the Tetris program, pass status on errors a bit better.

--- web/tetris.php
+++ web/tetris.php
@@ -1,38 +1,51 @@
-   <table width="320px">
-   <tr width="100%">
-       <td width="33%"></td>
-       <td width="33%">
-            <img id="rotate" src="rotate64.png" alt="Rotate">
-       </td>
-   </tr>
+<div id="tetris" class="insetcontent">
+    <div id="tetris_control">
+        <table width="100%">
+        <tr width="100%">
+           <td width="33%"></td>
+           <td width="33%">
+                <img id="rotate" src="rotate64.png" alt="Rotate">
+           </td>
+        </tr>
 
-    <tr width="100%">
-        <td width="33%">
-            <img id="left" src="left64.png" alt="Left">
-        </td>
-        <td width="33%">
-        </td>
-        <td width="33%">
-            <img id="right" src="right64.png" alt="Right">
-        </td>
-    </tr>
+        <tr width="100%">
+            <td width="33%">
+                <img id="left" src="left64.png" alt="Left">
+            </td>
+            <td width="33%">
+            </td>
+            <td width="33%">
+                <img id="right" src="right64.png" alt="Right">
+            </td>
+        </tr>
 
-    <tr width="100%">
-        <td width="33%">
-        </td>
-        <td width="33%">
-            <img id="drop" src="drop64.png" alt="Fire">
-        </td>
-        <td width="33%"> </td>
-    </tr>
-    </table>
+        <tr width="100%">
+            <td width="33%">
+            </td>
+            <td width="33%">
+                <img id="drop" src="drop64.png" alt="Fire">
+            </td>
+            <td width="33%"> </td>
+        </tr>
+        </table>
+    </div>
+
+    <div id="tetris_status">
+        <div class="iconrow">
+            <div title="Start Tetris running" onclick="run_command('tetris');" class="button green">Start</div>
+        </div>
+
+        <div id="tetrisstatusrow" class="statusrow">
+        </div>
+
+    </div>
+</div>
 
    <script src="jquery-1.6.4.js"></script>
    <script>
      $(document).ready(function(){
        $("img").click(function(event){
            send_command("fifo", "&fifo_name=myfifo&fifo_verb=" + event.target.id );
-         /*$.ajax({ url: "writefifo.php?cmd=" + event.target.id}); */
        });
      });
    </script>
--- web/webhelper.php
+++ web/webhelper.php
@@ -2,9 +2,13 @@
 
 function run_cmd($c)
 {
+    unset($output);
     $last = exec($c, $output, $rc);
+    $t = "Error - ";
+    foreach ($output as $o)
+        $t .= $o . " | ";
     if ($rc != 0)
-        header("HTTP/1.0 500 Error  - {$output[0]}");
+        header("HTTP/1.0 500 " . $t);
     else
         echo "$last";
 
@@ -91,41 +95,45 @@ function handle_fifo()
     }
     else if ($cmd == "displayon")
     {
-        run_cmd("../drive display --red=13");
+        run_cmd("../drive display --red=13 2>&1");
     }
 
     else if ($cmd == "displayoff")
     {
-        run_cmd("../drive displayoff");
+        run_cmd("../drive displayoff 2>&1");
     }
 
     else if ($cmd == "setmessage")
     {
-        run_cmd("MESSAGE='{$message}' ../makehelper");
+        run_cmd("MESSAGE='{$message}' ../makehelper 2>&1");
     }
 
     else if ($cmd == "init")
     {
-        $rc = run_cmd("../drive sync");
+        $rc = run_cmd("../drive sync 2>&1");
         if ($rc == 0)
-            $rc = run_cmd("../drive init");
+            $rc = run_cmd("../drive init 2>&1");
         if ($rc == 0)
             echo "Strings cleared (will also initialize).";
     }
     else if ($cmd == "fill")
     {
-        run_cmd("../drive flood");
+        run_cmd("../drive flood 2>&1");
         echo "Flooded all strings with color";
     }
     else if ($cmd == "chase")
     {
-        run_cmd("../drive chase");
+        run_cmd("../drive chase 2>&1");
+    }
+    else if ($cmd == "tetris")
+    {
+        run_cmd("DISPLAY=:0 ../tetrissim > /dev/null 2>&1 &");
     }
     else if ($cmd == "reset")
     {
-        run_cmd("stty --file /dev/ttyUSB0  hupcl ; \
-            (sleep 0.1 2>/dev/null || sleep 1) ; \
-                stty --file /dev/ttyUSB0 -hupcl ");
+        run_cmd("stty --file /dev/ttyUSB0  hupcl 2>&1 ; \
+            (sleep 0.1 2>/dev/null || sleep 1) 2>&1 ; \
+                stty --file /dev/ttyUSB0 -hupcl 2>&1 ");
         echo "Arduino reset.";
     }
     else if ($cmd == "fifo")
